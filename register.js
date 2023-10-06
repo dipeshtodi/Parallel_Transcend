ee58c1a5ee6df34359a4887adc5fb7261d96f05a
dipeshtodi <100477600+dipeshtodi@users.noreply.github.com>
2023-10-07 00:54:49 +0530

    register and login with authentication
    authenticated and dashboard

--- register.js
+++ register.js
@@ -1,13 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
+// Sample resource data (you would fetch this from a server in a real application)
+const resources = [
+    { title: "Tax Guide 2023", description: "A comprehensive guide to tax regulations for the year 2023." },
+    { title: "Tax Forms and Templates", description: "Downloadable tax forms and templates for various purposes." },
+    { title: "Tax Planning Strategies", description: "Strategies to optimize your tax planning and reduce liabilities." },
+    // Add more resource objects as needed
+];
 
-    signUpButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-    });
+// Function to create resource cards and display them
+function displayResources() {
+    const resourceList = document.getElementById("resource-list");
 
-    signInButton.addEventListener('click', () => {
-        container.classList.remove("right-panel-active");
+    resources.forEach((resource) => {
+        const resourceCard = document.createElement("div");
+        resourceCard.classList.add("resource-card");
+
+        const titleElement = document.createElement("div");
+        titleElement.classList.add("resource-title");
+        titleElement.textContent = resource.title;
+
+        const descriptionElement = document.createElement("div");
+        descriptionElement.classList.add("resource-description");
+        descriptionElement.textContent = resource.description;
+
+        resourceCard.appendChild(titleElement);
+        resourceCard.appendChild(descriptionElement);
+
+        resourceList.appendChild(resourceCard);
     });
-});
+}
+
+// Initial resource display
+displayResources();
